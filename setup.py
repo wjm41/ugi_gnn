ae62d1e62dd7d496dbb548e3964f60a83c90b50d
William McCorkindale <wjm41@cam.ac.uk>
2022-03-22 12:49:20 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -22,11 +22,11 @@ def get_version(rel_path):
 setup(
     name="dock2hit",
     version=get_version("dock2hit/__init__.py"),
-    description="solver for minimising intra-pair distances",
+    description="Module for pre-training GNNs on docking scores and predicting bioactivity.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/wjm41/ugi_gnn",
-    packages=find_packages(),
+    packages=['dock2hit'],
     author="W. McCorkindale",
     license="MIT License",
 )
