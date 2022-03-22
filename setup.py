093bdc5d2336cca813cf3da2d67427da3837c4af
William McCorkindale <wjm41@cam.ac.uk>
2022-03-22 15:42:13 +0000

    Initialised gitlab ci

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/wjm41/ugi_gnn",
+    python_requires='~=3.9, <=3.10',
     packages=['dock2hit'],
     author="W. McCorkindale",
     license="MIT License",
