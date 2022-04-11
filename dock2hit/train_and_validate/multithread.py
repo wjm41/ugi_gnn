2164f0181db7f121a79038db47315f6cf3dafce1
William McCorkindale <wjm41@cam.ac.uk>
2022-04-11 13:00:36 +0100

    implemented model configs

--- dock2hit/train_and_validate/multithread.py
+++ dock2hit/train_and_validate/multithread.py
@@ -14,7 +14,6 @@ from rdkit import Chem
 
 import torch
 from torch.nn import MSELoss
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 
 from dock2hit import parsing
@@ -25,25 +24,43 @@ from dock2hit.tensorboard_logging import Logger
 from dock2hit.utils import bash_command, human_len, get_device
 
 
-def train_and_validate(args, device):
+def log_minibatch_and_validate(logger, n_steps, train_loader, val_loader, validation_function):
+    # TODO fix y_scaler when using dataloader
+    batch_preds = train_loader.y_scaler.inverse_transform(
+        y_pred.cpu().detach().numpy().reshape(-1, 1))
+    batch_labs = train_loader.y_scaler.inverse_transform(
+        labels_mini.cpu().detach().reshape(-1, 1))
 
-    # load data (val_loader is None if no args.val and args.val_path is None)
-    train_loader, val_loader = load_data(args)
-    X_train, y_train = train_loader.df[args.smiles_col].values, train_loader.df[args.y_col].to_numpy(
-    )
+    # number of mols seen by model
+    if 'Felix' in args.optimizer:
+        logger.log(n_steps, loss.detach().item(), batch_preds, batch_labs,
+                   split='train', state_lrs=state_lrs)
+    else:
+        logger.log(n_steps, loss.detach().item(), batch_preds, batch_labs,
+                   split='train')
+    return
 
-    # Initialise featurisers
-    atom_featurizer = CanonicalAtomFeaturizer()
-    bond_featurizer = CanonicalBondFeaturizer()
 
-    e_feats = bond_featurizer.feat_size('e')
-    n_feats = atom_featurizer.feat_size('h')
+def validate_and_log(logger, n_steps, val_loader,  y_scaler, validation_function, scheduler):
 
-    # set up model
-    # mpnn_net = MPNNPredictor(node_in_feats=n_feats,
-    #                         edge_in_feats=e_feats,
-    #                         num_layer_set2set=6)
+    val_loss, val_preds, val_labs = validation_function(val_loader=val_loader,
+                                                        model=mpnn_net,
+                                                        atom_featurizer=atom_featurizer,
+                                                        bond_featurizer=bond_featurizer,
+                                                        loss_fn=loss_fn,
+                                                        device=device,
+                                                        y_scaler=train_loader.y_scaler)
+    scheduler.step(val_loss)
 
+    logger.log(n_steps, val_loss, val_preds, val_labs,
+               split='val')
+    return
+
+
+def configure_model(args, device):
+    model_config = {}
+
+    # load model
     mpnn_net = MPNNPredictor(node_in_feats=n_feats,
                              edge_in_feats=e_feats,
                              node_out_feats=32,
@@ -52,16 +69,25 @@ def train_and_validate(args, device):
                              num_step_set2set=2,
                              num_layer_set2set=3)
 
-    mpnn_net = mpnn_net.to(device)
+    mpnn_net.to(device)
+
+    # load optimizer
+    optimizer, scheduler = load_optimizer(args, mpnn_net)
+
+    # load featurizers
+    atom_featurizer = CanonicalAtomFeaturizer()
+    bond_featurizer = CanonicalBondFeaturizer()
+
+    e_feats = bond_featurizer.feat_size('e')
+    n_feats = atom_featurizer.feat_size('h')
 
     logging.info('Number of model parameters: {}'.format(sum(p.numel()
                                                              for p in mpnn_net.parameters() if p.requires_grad)))
     logging.info(f'Number of node features: {n_feats}')
     logging.info(f'Number of edge features: {e_feats}')
 
-    optimizer = load_optimizer(args, mpnn_net)
-
-    scheduler = ReduceLROnPlateau(optimizer, 'min')
+    # load loss function
+    loss_fn = MSELoss()
 
     if args.path_to_load_checkpoint is not None:
         checkpoint = torch.load(
@@ -75,11 +101,27 @@ def train_and_validate(args, device):
         start_epoch = 0
         start_batch = 0
 
-    loss_fn = MSELoss()
+    model_config['model'] = mpnn_net
+    model_config['optimizer'] = optimizer
+    model_config['scheduler'] = scheduler
+    model_config['atom_featurizer'] = atom_featurizer
+    model_config['bond_featurizer'] = bond_featurizer
+    model_config['loss_fn'] = loss_fn
+    model_config['start_epoch'] = start_epoch
+    model_config['start_batch'] = start_batch
+    return model_config
+
+
+def train_and_validate(args, device):
+
+    # val_loader is None if no args.val and args.val_path is None
+    train_loader, val_loader = load_data(args)
+    model_config = configure_model(args, device)
 
     logging.info('beginning training...')
     if args.log_dir is not None:
         logger = Logger(args)
+
     n_steps = 0
     for epoch in range(start_epoch, args.n_epochs):
         mpnn_net.train()
@@ -108,7 +150,7 @@ def train_and_validate(args, device):
                 batch_data = list(zip(graphs, labels))
                 batch_loader = DataLoader(
                     batch_data, batch_size=args.minibatch_size, shuffle=True, collate_fn=collate, drop_last=False, num_workers=4)
-                n_mini = 0
+                time_to_log = False
 
                 while not task.done():
                     # infinite looping
@@ -136,42 +178,16 @@ def train_and_validate(args, device):
                     else:
                         optimizer.step()
 
-                    n_mini += 1
-                # print(f'Number of minibatches = {n_mini}, \
-                #         Number of molecules = {n_mini*args.minibatch_size} \
-                #             ({100*n_mini*args.minibatch_size/args.batch_size:.1f}%)')
-                n_steps += n_mini
+                    n_steps += 1
+                    if n_steps % args.steps_per_log == 0:
+                        time_to_log = True
 
-                # TODO update batch num with minibatch num
-                if n_steps % args.steps_per_log == 0 and args.log_dir is not None:
-
-                    # TODO fix y_scaler when using dataloader
-                    batch_preds = train_loader.y_scaler.inverse_transform(
-                        y_pred.cpu().detach().numpy().reshape(-1, 1))
-                    batch_labs = train_loader.y_scaler.inverse_transform(
-                        labels_mini.cpu().detach().reshape(-1, 1))
-
-                    # number of mols seen by model
-                    if 'Felix' in args.optimizer:
-                        logger.log(n_steps, loss.detach().item(), batch_preds, batch_labs,
-                                   split='train', state_lrs=state_lrs)
-                    else:
-                        logger.log(n_steps, loss.detach().item(), batch_preds, batch_labs,
-                                   split='train')
-
+                # TODO wrap as its own function
+                if time_to_log and args.log_dir is not None:
+                    log_minibatch_and_validation_set()
                     if val_loader is not None:
-
-                        val_loss, val_preds, val_labs = multi_validate(val_loader=val_loader,
-                                                                       model=mpnn_net,
-                                                                       atom_featurizer=atom_featurizer,
-                                                                       bond_featurizer=bond_featurizer,
-                                                                       loss_fn=loss_fn,
-                                                                       device=device,
-                                                                       y_scaler=train_loader.y_scaler)
-                        scheduler.step(val_loss)
-
-                        logger.log(n_steps, val_loss, val_preds, val_labs,
-                                   split='val')
+                        validate_and_log(
+                            logger, n_steps, train_loader, val_loader, validate)
 
                 if batch_num % args.batches_per_save == 0 and args.save_dir is not None:
                     if not os.path.isdir(args.save_dir):
